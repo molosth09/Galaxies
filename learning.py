288a73228fd82e8dee4974020cad24362d0e63dc
molosth09 <128758294+molosth09@users.noreply.github.com>
2023-03-27 23:08:02 +0200

    Add files via upload

---
+++ learning.py
@@ -0,0 +1,15 @@
+if 1+1==3:
+     print("true")
+else:
+     print("false")
+
+if 2 < 5:
+     print("true")
+else:
+     print("false")
+
+if 1==2:
+     print("NO")
+else:
+     print("YES")
+    
